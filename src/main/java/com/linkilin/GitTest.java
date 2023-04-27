c1291ea922b896cfeaf87b0839946071146ea888
LinKiLin <linxugo@163.com>
2023-04-27 15:12:44 +0800

    hot-fix first commit

--- src/main/java/com/linkilin/GitTest.java
+++ src/main/java/com/linkilin/GitTest.java
@@ -3,5 +3,6 @@ package com.linkilin;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("master first commit");
+        System.out.println("hot-fix first commit");
     }
 }
