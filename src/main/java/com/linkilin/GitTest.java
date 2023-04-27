431fc6b44e515723e693f22b9b0569fabe821da5
LinKiLin <linxugo@163.com>
2023-04-27 15:55:24 +0800

    hot-fix push second test

--- src/main/java/com/linkilin/GitTest.java
+++ src/main/java/com/linkilin/GitTest.java
@@ -3,6 +3,7 @@ package com.linkilin;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("master first commit");
+        System.out.println("testingggggg");
         System.out.println("hot-fix first commit");
         System.out.println("hot-fix push test");
     }
