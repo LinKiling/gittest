ee37817523dfdaa4f4b19cc978dd310ce7c98725
LinKiLin <linxugo@163.com>
2023-04-27 15:52:45 +0800

    push second test

--- src/main/java/com/linkilin/GitTest.java
+++ src/main/java/com/linkilin/GitTest.java
@@ -2,9 +2,6 @@ package com.linkilin;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("master first commit");
-        System.out.println("master second commit");
-        System.out.println("hot-fix first commit");
-        System.out.println("master push test");
+        System.out.println("Testingggggggggg");
     }
 }
