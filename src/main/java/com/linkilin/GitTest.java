42d4433d22773e7eff82ef6f8351a0611b0d1077
LinKiLin <linxugo@163.com>
2023-04-27 15:11:48 +0800

    master second commit

--- src/main/java/com/linkilin/GitTest.java
+++ src/main/java/com/linkilin/GitTest.java
@@ -3,5 +3,6 @@ package com.linkilin;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("master first commit");
+        System.out.println("master second commit");
     }
 }
